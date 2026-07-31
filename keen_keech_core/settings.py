5bc4033a4a9fea08203bcf3cf87b77062261b9d2
Udaya Nidhi <udayanidhi333@gmail.com>
2026-08-01 05:15:47 +0530

    Revert to compatible Cloudinary storage settings

--- keen_keech_core/settings.py
+++ keen_keech_core/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -159,14 +159,7 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
 
-STORAGES = {
-    "default": {
-        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
-    },
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # =========================================================
 # GMAIL SMTP EMAIL BACKEND CONFIGURATION
