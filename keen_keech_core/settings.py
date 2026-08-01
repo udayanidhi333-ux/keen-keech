270f6e5bed055a4d961c8fa4c2cfc694c7bd2e3a
Udaya Nidhi <udayanidhi333@gmail.com>
2026-08-01 12:18:31 +0530

    Fix checkout crash: add email timeout and safe error handling

--- keen_keech_core/settings.py
+++ keen_keech_core/settings.py
@@ -178,6 +178,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-relay.brevo.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_TIMEOUT = 5
 
 
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
